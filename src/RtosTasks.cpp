c332194857dd0d47358b2bae339cab1590eff0ca
Achim <achim.schneider@t-online.de>
2025-08-19 15:40:50 +0200

    typo corrected

--- src/RtosTasks.cpp
+++ src/RtosTasks.cpp
@@ -47,96 +47,96 @@ void parserTask(void* pvParameters) {
             }
         }
     }
+}
+// ----------------------------
+// freeRTOS Task Registry
+void twinRegister(void* pvParameters) {
+    Register = new FlapRegistry();
+    Register->repairOutOfPoolDevices();                                         // repair devices that are out of the address pool
+    Register->registerUnregistered();                                           // register all twins which are not registered yet
+    Register->scan_i2c_bus();                                                   // scan i2c bus for new twins
 
-    // ----------------------------
-    // freeRTOS Task Registry
-    void twinRegister(void* pvParameters) {
-        Register = new FlapRegistry();
-        Register->repairOutOfPoolDevices();                                     // repair devices that are out of the address pool
-        Register->registerUnregistered();                                       // register all twins which are not registered yet
-        Register->scan_i2c_bus();                                               // scan i2c bus for new twins
+    #ifdef DISABLEREGISTRY
+        {
+        TraceScope trace;                                                       // use semaphore to protect this block
+        masterPrintln("Registry is disabled, no scan and no registry");
+        vTaskSuspend(nullptr);                                                  // suspend task,  do not use registry
+        return;
+        }
+    #endif
 
-        #ifdef DISABLEREGISTRY
-            {
+    shortScanTimer =
+        xTimerCreate("ScanShort", pdMS_TO_TICKS(SHORT_SCAN_COUNTDOWN), pdTRUE, nullptr, shortScanCallback); // 1) Short-Scan-Timer (Auto-Reload)
+    longScanTimer =
+        xTimerCreate("ScanLong", pdMS_TO_TICKS(LONG_SCAN_COUNTDOWN), pdTRUE, nullptr, longScanCallback); // 2) Long-Scan-Timer (Auto-Reload)
+    availCheckTimer = xTimerCreate("AvailChk", pdMS_TO_TICKS(AVAILABILITY_CHECK_COUNTDOWN), pdTRUE, nullptr,
+                                   availCheckCallback);                         // 3) Availability-Check-Timer (Auto-Reload)
+
+    vTaskDelay(pdMS_TO_TICKS(1 * 60 * 1000));                                   // start Availability-Check with 1 Min-Offset
+    xTimerStart(availCheckTimer, 0);
+
+    xTimerStart(shortScanTimer, 0);                                             // starte Short-Scan now
+    vTaskSuspend(nullptr);                                                      // suspend task,  Callbacks are doing the job
+}
+
+// ----------------------------
+// freeRTOS Task Twin[index]
+void slaveTwinTask(void* pvParameters) {
+    int twinIndex = *(int*)pvParameters;                                        // Twin Number
+    Twin[twinIndex]->createQueue();                                             // Create twin Queue
+
+    while (true) {
+        Twin[twinIndex]->readQueue();                                           // read event/command from twin Queue
+    }
+}
+
+// ----------------------------
+// freeRTOS Task Statistic
+void statisticTask(void* param) {
+    TickType_t lastWakeTime = xTaskGetTickCount();                              // get current tick count
+    DataEvaluation          = new FlapStatistics();                             // create Object for statistic task
+    while (true) {
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(60000));                   // every 1 Minute
+        {
             TraceScope trace;                                                   // use semaphore to protect this block
-            masterPrintln("Registry is disabled, no scan and no registry");
-            vTaskSuspend(nullptr);                                              // suspend task,  do not use registry
-            return;
+            if (DataEvaluation != nullptr) {
+                DataEvaluation->increment();                                    // increment statistic counter
+                #ifdef STATISTICVERBOSE
+                    masterPrintln("I²C statistic cycle - access: ", DataEvaluation->_busAccessCounter, " data write: ", DataEvaluation->_busDataCounter,
+                    " data read: ", DataEvaluation->_busReadCounter);
+                #endif
             }
-        #endif
-
-        shortScanTimer =
-            xTimerCreate("ScanShort", pdMS_TO_TICKS(SHORT_SCAN_COUNTDOWN), pdTRUE, nullptr, shortScanCallback); // 1) Short-Scan-Timer (Auto-Reload)
-        longScanTimer =
-            xTimerCreate("ScanLong", pdMS_TO_TICKS(LONG_SCAN_COUNTDOWN), pdTRUE, nullptr, longScanCallback); // 2) Long-Scan-Timer (Auto-Reload)
-        availCheckTimer = xTimerCreate("AvailChk", pdMS_TO_TICKS(AVAILABILITY_CHECK_COUNTDOWN), pdTRUE, nullptr,
-                                       availCheckCallback);                     // 3) Availability-Check-Timer (Auto-Reload)
-
-        vTaskDelay(pdMS_TO_TICKS(1 * 60 * 1000));                               // start Availability-Check with 1 Min-Offset
-        xTimerStart(availCheckTimer, 0);
-
-        xTimerStart(shortScanTimer, 0);                                         // starte Short-Scan now
-        vTaskSuspend(nullptr);                                                  // suspend task,  Callbacks are doing the job
-    }
-
-    // ----------------------------
-    // freeRTOS Task Twin[index]
-    void slaveTwinTask(void* pvParameters) {
-        int twinIndex = *(int*)pvParameters;                                    // Twin Number
-        Twin[twinIndex]->createQueue();                                         // Create twin Queue
-
-        while (true) {
-            Twin[twinIndex]->readQueue();                                       // read event/command from twin Queue
         }
+        DataEvaluation->makeHistory();                                          // transfer counter to next history cycle
     }
+}
 
-    // ----------------------------
-    // freeRTOS Task Statistic
-    void statisticTask(void* param) {
-        TickType_t lastWakeTime = xTaskGetTickCount();                          // get current tick count
-        DataEvaluation          = new FlapStatistics();                         // create Object for statistic task
-        while (true) {
-            vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(60000));               // every 1 Minute
-            {
-                TraceScope trace;                                               // use semaphore to protect this block
-                if (DataEvaluation != nullptr) {
-                    DataEvaluation->increment();                                // increment statistic counter
-                    #ifdef STATISTICVERBOSE
-                        masterPrintln("I²C statistic cycle - access: ", DataEvaluation->_busAccessCounter,
-                        " data write: ", DataEvaluation->_busDataCounter, " data read: ", DataEvaluation->_busReadCounter);
-                    #endif
-                }
-            }
-            DataEvaluation->makeHistory();                                      // transfer counter to next history cycle
+// ----------------------------
+// freeRTOS Task Reporting
+void reportTask(void* pvParameters) {
+    Key21          receivedKey;
+    ReportCommands receivedCmd;
+
+    FlapReporting* Reports;                                                     // create object for task
+    g_reportQueue = xQueueCreate(1, sizeof(ReportCommand));                     // Create task Queue
+
+    while (true) {
+        if (xQueueReceive(g_reportQueue, &receivedCmd, portMAX_DELAY)) {        // wait for Queue message
+            Reports->reportPrintln("======== Flap Master Health Overview ========"); // Report Header
+            if (receivedCmd == REPORT_TASKS_STATUS)
+                Reports->reportTaskStatus();                                    // uptime and scheduled scans
+            if (receivedCmd == REPORT_MEMORY)
+                Reports->reportMemory();                                        // ESP32 (RAM status)
+            if (receivedCmd == REPORT_RTOS_TASKS)
+                Reports->reportRtosTasks();                                     // show Task List
+            if (receivedCmd == REPORT_STEPS_BY_FLAP)
+                Reports->reportAllTwinStepsByFlap();                            // show Slave steps prt Flap
+            if (receivedCmd == REPORT_REGISTRY)
+                Reports->reportSlaveRegistry();                                 // show registry
+            if (receivedCmd == REPORT_I2C_STATISTIC)
+                Reports->reportI2CStatistic();                                  // shoe I2C usage
         }
+        Reports->reportPrintln("======== Flap Master Health Overview End ====");
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
-
-    // ----------------------------
-    // freeRTOS Task Reporting
-    void reportTask(void* pvParameters) {
-        Key21          receivedKey;
-        ReportCommands receivedCmd;
-
-        FlapReporting* Reports;                                                 // create object for task
-        g_reportQueue = xQueueCreate(1, sizeof(ReportCommand));                 // Create task Queue
-
-        while (true) {
-            if (xQueueReceive(g_reportQueue, &receivedCmd, portMAX_DELAY)) {    // wait for Queue message
-                Reports->reportPrintln("======== Flap Master Health Overview ========"); // Report Header
-                if (receivedCmd == REPORT_TASKS_STATUS)
-                    Reports->reportTaskStatus();                                // uptime and scheduled scans
-                if (receivedCmd == REPORT_MEMORY)
-                    Reports->reportMemory();                                    // ESP32 (RAM status)
-                if (receivedCmd == REPORT_RTOS_TASKS)
-                    Reports->reportRtosTasks();                                 // show Task List
-                if (receivedCmd == REPORT_STEPS_BY_FLAP)
-                    Reports->reportAllTwinStepsByFlap();                        // show Slave steps prt Flap
-                if (receivedCmd == REPORT_REGISTRY)
-                    Reports->reportSlaveRegistry();                             // show registry
-                if (receivedCmd == REPORT_I2C_STATISTIC)
-                    Reports->reportI2CStatistic();                              // shoe I2C usage
-            }
-            Reports->reportPrintln("======== Flap Master Health Overview End ====");
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-    }
+}
